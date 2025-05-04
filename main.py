ecb359153ff5a245d568a31dbdea6d5de5e60c34
Athos <athosbds@gmail.com>
2025-05-03 21:30:09 -0300

    mod

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ button_1 = Button(frame_body, text="C", width=11, height=2, bg=colors['Cizenta']
 button_1.place(x=0, y=0)
 button_2 = Button(frame_body, text="%", width=5, height=2, bg=colors["Cizenta"], font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 button_2.place(x=118, y=0)
-button_3 = Button(frame_body, text="/", width=5, height=2, bg=colors['Laranja'], fg=colors['Branca'], font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+button_3 = Button(frame_body, text="÷", width=5, height=2, bg=colors['Laranja'], fg=colors['Branca'], font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 button_3.place(x=177, y=0)
 
 # BOTÕES - SEGUNDA FILEIRA
